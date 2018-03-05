989eec319797fb0df499b71f24d15ace7081d6db
Louis Pan <louis@pan.me>
2018-03-05 22:03:03 +1100

    v0.2.0.0 Using Kleisli functions

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 [![Build Status](https://secure.travis-ci.org/louispan/arrow-extras.png?branch=master)](http://travis-ci.org/louispan/arrow-extras)
 
 Extra functions for Control.Arrow
+
+* 0.2.0.0
+  - Deleted previous module (they weren't useful).
+  - Replaced with fuctions to manipulate monads under 'Keisli'.
+
+* pre 0.1.0.0
+  - Initial version.
--- src/Control/Arrow/Extras.hs
+++ src/Control/Arrow/Extras.hs
@@ -2,41 +2,63 @@ module Control.Arrow.Extras where
 
 import Control.Arrow
 
-infixl 0 >*>
-infixr 1 $>>, >>$
-infixr 1 $<<, <<$
+-- | 'arr' under 'Kleisli'
+arrK :: Monad m => (b -> c) -> (b -> m c)
+arrK f = runK (arr f)
 
--- | Feed the output of second arrow into the output of the first arrow.
--- Can be used to emulate Applicative <*> like usage:
---
--- @
--- foo :: Appplicative f => f a -> f b -> f (a, b)
--- (,) <$> a <*> b
--- @
---
--- The arrow version is:
---
--- @
--- foo :: Arrow p => p x a -> p x b -> p x (a, b)
--- arr (const (,)) >*> a >*> b
--- @
-(>*>) :: Arrow p => p a (b -> c)
-    -> p a b
-    -> p a c
-af >*> ab = af &&& ab >>^ (fst <*> snd)
+-- | 'first' under 'Kleisli'
+firstK :: Monad m => (b -> m c) -> ((b, d) -> m (c, d))
+firstK = underK1 first
 
--- | precomposition with a pure value
-(<<$) :: Arrow a => a c d -> c -> a x d
-p <<$ v = p <<^ const v
+-- | 'second' under 'Kleisli'
+secondK :: Monad m => (b -> m c) -> ((d, b) -> m (d, c))
+secondK = underK1 second
 
--- | postcomposition with a pure value
-(>>$) :: Arrow a => a b c -> d -> a b d
-p >>$ v = p >>^ const v
+-- | '***' under 'Kleisli'
+-- The @|<@ looks like a @K@
+(***|<) :: Monad m => (b -> m c) -> (b' -> m c') -> ((b, b') -> m (c, c'))
+(***|<) = underK2 (***)
+infixr 3 ***|< -- like ***
 
--- | postcomposition with a pure value (right-to-left variant)
-($<<) :: Arrow a => d -> a b c -> a b d
-v $<< p = const v ^<< p
+-- | '&&&' under 'Kleisli'
+-- The @|<@ looks like a @K@
+(&&&|<) :: Monad m => (b -> m c) -> (b -> m c') -> (b -> m (c, c'))
+(&&&|<) = underK2 (&&&)
+infixr 3 &&&|< -- like (&&&)
 
--- | precomposition with a pure value (right-to-left variant)
-($>>) :: Arrow a => c -> a c d -> a x d
-v $>> p = const v ^>> p
+-- | 'left' under 'Kleisli'
+leftK :: Monad m => (b -> m c) -> (Either b d -> m (Either c d))
+leftK = underK1 left
+
+-- | 'right' under 'Kleisli'
+rightK :: Monad m => (b -> m c) -> (Either d b -> m (Either d c))
+rightK = underK1 right
+
+-- | '+++' under 'Kleisli'
+-- The @|<@ looks like a @K@
+(+++|<) :: Monad m =>  (b -> m c) -> (b' -> m c')
+    -> (Either b b' -> m (Either c c'))
+(+++|<) = underK2 (+++)
+infixr 2 +++|< -- like (+++)
+
+-- | '+++' under 'Kleisli'
+-- The @|<@ looks like a @K@
+(||||<) :: Monad m => (b -> m d) -> (c -> m d) -> (Either b c -> m d)
+(||||<) = underK2 (|||)
+infixr 2 ||||< -- like (+++)
+
+-- | shorter name for 'runKleisli'
+runK :: Kleisli m a1 b1
+    -> (a1 -> m b1)
+runK = runKleisli
+
+-- | run under 'Kleisli'
+underK1 :: (Kleisli m a1 b1 -> Kleisli m a2 b2)
+    -> (a1 -> m b1) -> (a2 -> m b2)
+underK1 f x = runKleisli $ f (Kleisli x)
+
+-- | run binary function under 'Kleisli'
+underK2 :: (Kleisli m a1 b1 -> Kleisli m a2 b2 -> Kleisli m a3 b3)
+    -> (a1 -> m b1) -> (a2 -> m b2) -> (a3 -> m b3)
+underK2 f x y = runKleisli $ f (Kleisli x) (Kleisli y)
+
